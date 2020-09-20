f360e355a9d78469e605502b3dfbc4bc66474920
tabatha wiggers <tatisabato@hotmail.com>
2020-09-20 11:51:32 -0300

    learned front-end

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,8 +2,8 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Change your life", "Learn to code"],
-    typeSpeed: 50,
+    strings: ["Change your life", "Learn to code ;)"],
+    typeSpeed: 60,
     loop: true
   });
 }
